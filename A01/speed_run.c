56acd5296b7979e683e6fddc64829a0f05759a83
Bárbara Galiza <barnobregali02@gmail.com>
2022-11-15 13:03:38 +0000

    added solution 2

--- A01/speed_run.c
+++ A01/speed_run.c
@@ -72,9 +72,9 @@ solution_t;
 //
 //
 
-static solution_t solution_1,solution_1_best, solution_2,solution_2_best, solution_3,solution_3_best;
-static double solution_1_elapsed_time, solution_2_elapsed_time, solution_3_elapsed_time; // time it took to solve the problem
-static unsigned long solution_1_count, solution_2_count, solution_3_count; // effort dispended solving the problem
+static solution_t solution_1,solution_1_best,solution_2,solution_2_best; //solution_3,solution_3_best;
+static double solution_1_elapsed_time, solution_2_elapsed_time; // solution_3_elapsed_time; // time it took to solve the problem
+static unsigned long solution_1_count, solution_2_count; // solution_3_count; // effort dispended solving the problem
 
 static void solution_1_recursion(int move_number,int position,int speed,int final_position)
 {
@@ -122,8 +122,50 @@ static void solve_1(int final_position)
 //
 // solution 2
 //
+static void solution_2_recursion(int move_number,int position,int speed,int final_position)
+{
+  int i,new_speed;
+
+  // record move
+  solution_2_count++;
+  solution_2.positions[move_number] = position;
+  // is it a solution?
+  if(position == final_position && speed == 1)
+  {
+    // is it a better solution?
+    if(move_number < solution_2_best.n_moves)
+    {
+      solution_2_best = solution_2;
+      solution_2_best.n_moves = move_number;
+    }
+    return;
+  }
+  // no, try all legal speeds
+  if( solution_2_best.positions[move_number] > solution_2.positions[move_number]) return ;
+  for(new_speed = speed + 1; new_speed >= speed - 1; new_speed--)
+    if(new_speed >= 1 && new_speed <= _max_road_speed_ && position + new_speed <= final_position)
+    {
+      for(i = 0;i <= new_speed && new_speed <= max_road_speed[position + i];i++) 
+        ;
+      if(i > new_speed)
+        solution_2_recursion(move_number + 1,position + new_speed,new_speed,final_position);
+    }
+}
 
 
+static void solve_2(int final_position)
+{
+  if(final_position < 1 || final_position > _max_road_size_)
+  {
+    fprintf(stderr,"solve_2: bad final_position\n");
+    exit(1);
+  }
+  solution_2_elapsed_time = cpu_time();
+  solution_2_count = 0ul;
+  solution_2_best.n_moves = final_position + 100;
+  solution_2_recursion(0,0,0,final_position);
+  solution_2_elapsed_time = cpu_time() - solution_2_elapsed_time;
+}
 
 //
 // example of the slides
@@ -182,7 +224,7 @@ int main(int argc,char *argv[argc + 1])
     printf("%3d |",final_position);
 
     // first solution method (very bad)
-    if(solution_1_elapsed_time < _time_limit_)
+    /* if(solution_1_elapsed_time < _time_limit_)
     {
       solve_1(final_position);
       if(print_this_one != 0)
@@ -196,10 +238,10 @@ int main(int argc,char *argv[argc + 1])
     {
       solution_1_best.n_moves = -1;
       printf("                                |");
-    }
+    } */
 
     // second solution method (less bad)
-   /*  if(solution_2_elapsed_time < _time_limit_)
+    if(solution_2_elapsed_time < _time_limit_)
     {
       solve_2(final_position);
       if(print_this_one != 0)
@@ -213,7 +255,7 @@ int main(int argc,char *argv[argc + 1])
     {
       solution_2_best.n_moves = -1;
       printf("                                |");
-    } */
+    }
 
     // third solution method
   /*   if(solution_3_elapsed_time < _time_limit_)
